543be7a0b230581893dc00b2751bba71193444c4
Angel Daniel Aviles cruz <angeldaniel.avilescruz66@gmail.com>
2021-03-02 14:21:56 -0600

    difinicion de rutas y el menu

---
+++ src/app/mapas/mapas-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FullScreenComponent } from './pages/full-screen/full-screen.component';
+import { MarcadoresComponent } from './pages/marcadores/marcadores.component';
+import { ZoomRageComponent } from './pages/zoom-rage/zoom-rage.component';
+import { PropiedadesComponent } from './pages/propiedades/propiedades.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    children:[
+      {path: 'fullscreen', component: FullScreenComponent},
+      {path: 'zoom-range', component: ZoomRageComponent},
+      {path: 'marcadores', component: MarcadoresComponent},
+      {path: 'propiedades', component: PropiedadesComponent},
+      {path: '**', redirectTo:'fullscreen'}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MapasRoutingModule { }
